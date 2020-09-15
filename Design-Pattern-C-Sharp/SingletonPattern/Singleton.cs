aa7030c3f54f6e6dd62ab7b431c90f9ed97b8715
Rafael Batista <maranata_rafa@hotmail.com>
2020-09-14 23:39:09 -0400

    Foi criado padrão Singleton

---
+++ Design-Pattern-C-Sharp/SingletonPattern/Singleton.cs
@@ -0,0 +1,14 @@
+﻿namespace Design_Pattern_C_Sharp.SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton _intancia = null;
+
+        public static Singleton Instancia => ObterInstancia();
+
+        private static Singleton ObterInstancia()
+        {
+            return _intancia ??= new Singleton();
+        }
+    }
+}
