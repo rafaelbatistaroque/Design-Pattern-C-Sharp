876c8673f1a3d4c9cb2eeb5a0aa42534e1e5d1a8
Rafael Batista <maranata_rafa@hotmail.com>
2020-09-14 22:25:45 -0400

    Adicionar arquivos de projeto.

---
+++ Design-Pattern-C-Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design_Pattern_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
